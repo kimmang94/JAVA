19086fcbbe8cccc03e0560fd4dc769fb84c102f3
KIMHYEOK\borad <kimmang940715@naver.com>
2025-02-17 22:54:57 +0900

    산술 연산자

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,33 +2,30 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-       int x = 10;
-       int y = 10;
-       int z;
+       /*
+       산술 연산자
+       + : 더하기 ; 덧셈 연산
+       - : 빼기 ; 뺼셈 연산
+       * : 곱하기 ; 곱셈 연산
+       / : 나누기 ; 나눗셈 연산
+       % : 나머지 ; 나눗셈의 나머지를 산출하는 연산
+        */
 
-       x++;
-       ++x;
-        System.out.println("x = " + x);
+        byte v1 = 10;
+        byte v2 = 4;
+        int v3 = 5;
+        long v4 = 10L;
 
-        System.out.println("================");
-        y--;
-        --y;
+        int result1 = v1 + v2;
+        System.out.println("result1 :" + result1);
 
-        System.out.println("y = " + y);
-        System.out.println("================");
-        z = x++;
-        System.out.println("z = " + z);
-        System.out.println("x = " + x);
+        long result2 = v1 + v2 - v4;
+        System.out.println("result2 :" + result2);
 
-        System.out.println("================");
-        z = ++x;
-        System.out.println("z ="  + z);
-        System.out.println("x = " + x);
+        double result3 = (double)v1 / v2;
+        System.out.println("result3 :" + result3);
 
-        System.out.println("================");
-        z = ++x + y++;
-        System.out.println("z = " +  z);
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
+        int result4 = v1 % v2;
+        System.out.println("result4 :" + result4);
     }
 }
