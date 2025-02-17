fb6a55a855eeb24567366b98de55bb93b6db76b7
KIMHYEOK\borad <kimmang940715@naver.com>
2025-02-17 22:11:18 +0900

    변수, 정수타입

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,15 +2,29 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+        // 변수 선언
+        int score = 100;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
-            System.out.println("Hello World!");
-        }
+        System.out.println(score);
+
+
+        // 정수 타입
+        /*
+        정수 : byte, char, short, int, long
+        실수 : float, double
+        논리 : boolean
+         */
+
+        byte var1 = -128;
+        byte var2 = -30;
+        byte var3 = 0;
+        byte var4 = 30;
+        byte var5 = 127;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
     }
 }
