42f23d306373ab2cd3ac2b97105d7ee7942a9dee
KIMHYEOK\borad <kimmang940715@naver.com>
2025-02-17 22:49:46 +0900

    연산자

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,29 +2,33 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        // 변수 선언
-        int score = 100;
+       int x = 10;
+       int y = 10;
+       int z;
 
-        System.out.println(score);
+       x++;
+       ++x;
+        System.out.println("x = " + x);
 
+        System.out.println("================");
+        y--;
+        --y;
 
-        // 정수 타입
-        /*
-        정수 : byte, char, short, int, long
-        실수 : float, double
-        논리 : boolean
-         */
+        System.out.println("y = " + y);
+        System.out.println("================");
+        z = x++;
+        System.out.println("z = " + z);
+        System.out.println("x = " + x);
 
-        byte var1 = -128;
-        byte var2 = -30;
-        byte var3 = 0;
-        byte var4 = 30;
-        byte var5 = 127;
+        System.out.println("================");
+        z = ++x;
+        System.out.println("z ="  + z);
+        System.out.println("x = " + x);
 
-        System.out.println(var1);
-        System.out.println(var2);
-        System.out.println(var3);
-        System.out.println(var4);
-        System.out.println(var5);
+        System.out.println("================");
+        z = ++x + y++;
+        System.out.println("z = " +  z);
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
     }
 }
