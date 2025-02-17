55685bab8e2693e3896fb44aa77bf88438c59053
KIMHYEOK\borad <kimmang940715@naver.com>
2025-02-17 23:16:25 +0900

    정수연산으로 변경

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         /*
         정확한 계산은 정수 연산
          */
-
         int apple = 1;
-        double pieceUnit = 0.1;
+        int totalPieces = apple * 10;
         int number = 7;
 
-        double result = apple - number * pieceUnit;
-        System.out.println("사과 1개에서 남은 양 : " + result);
+        int result = totalPieces - number;
+        System.out.println("10 조각에서 남은 조각 : " + result);
+        System.out.println("사과 1개에서 남은 양 : " + result/10.0);
     }
 }
