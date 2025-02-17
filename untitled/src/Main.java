17994760449df23e56239efe29137d605eb0b427
KIMHYEOK\borad <kimmang940715@naver.com>
2025-02-18 00:06:47 +0900

    isInfinite 와 isNaN

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,14 +3,19 @@
 public class Main {
     public static void main(String[] args) {
         /*
-        정확한 계산은 정수 연산
+        isInfinite 와 isNaN
          */
-        int apple = 1;
-        int totalPieces = apple * 10;
-        int number = 7;
+        int x = 5;
+        double y = 0.0;
+        double z =  x / y;
+        //int result = 5 / 0; 예외 발생
+        System.out.println(z + 2);
 
-        int result = totalPieces - number;
-        System.out.println("10 조각에서 남은 조각 : " + result);
-        System.out.println("사과 1개에서 남은 양 : " + result/10.0);
+        if (Double.isInfinite(z) || Double.isNaN(z)) {
+            System.out.println("값 산출 불가");
+        } else {
+            System.out.println(z + 2);
+        }
+
     }
 }
