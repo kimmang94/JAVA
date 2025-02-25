bbb2b195e99c95faf0498a8c0492f4b109f62713
KIMHYEOK\borad <kimmang940715@naver.com>
2025-02-25 22:12:51 +0900

    데이터 타입

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,20 +2,28 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        /*
-        isInfinite 와 isNaN
-         */
-        int x = 5;
-        double y = 0.0;
-        double z =  x / y;
-        //int result = 5 / 0; 예외 발생
-        System.out.println(z + 2);
+       /*
+            1. 타입
+                1) 기본 타입
+                정의 : 기본 타입은 산수 및 수학에서 사용하는 데이터이자 값 그 자체이다
+                선언 방법 : 저장할 때 용도를 구분하고 표시하기 위해 타입 을 앞에 선언한다
 
-        if (Double.isInfinite(z) || Double.isNaN(z)) {
-            System.out.println("값 산출 불가");
-        } else {
-            System.out.println(z + 2);
-        }
+                boolean 타입 : 참이나 거짓 저장
+                char 타입 : 문자(한 글자) 저장
+                int 타입 : 정수(소수점이 없는 수) 저장
+                double 타입 : 실수(소수점이 있는 수) 저장
+                String 타입 : 문자열(여러 문자) 저장 (참조 타입)
+                
+        */
 
+        boolean isL = false;
+        char sult = 'H';
+        int year = 2025;
+        int month = 6;
+        
+        double latitude = 37.52127220511242;
+        double lingitude = 127.0074462890625;
+        
+        String card = "H8"; // 문자열 참조 타입
     }
 }
