9ce14470a4f7de9e38302789e01e52ebe89ba6b2
KIMHYEOK\borad <kimmang940715@naver.com>
2025-02-25 22:23:40 +0900

    반복문 까지 정리

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -25,5 +25,72 @@ public class Main {
         double lingitude = 127.0074462890625;
         
         String card = "H8"; // 문자열 참조 타입
+
+    /*
+        프로그램 실행 순서
+        - 프로그램은 위에서 아래로 순차적 (Sequence)으로 실행된다.
+        - 프로그램의 순서를 제어하기 위한 조건문에는 if, switch, 삼항연잔가(?:) 가 있다.
+        - 프로그램의 특정 부분을 반복시키는 반복문에는 for, while, do~while 이 있다.
+     */
+        int v = 100;
+        if (v % 2 == 1) {
+            System.out.println("짝수");
+        }
+        else {
+            System.out.println("홀수");
+        }
+
+        // 삼항 연산자
+        int temp = 99;
+        temp = (temp % 2 == 1)? temp * 3 + 1 : temp / 2;
+
+        int tot = 0;
+        char c = 'A';
+        switch (c) {
+            case 'A':
+                tot = 1;
+                break;
+            case 'B':
+                tot = 2;
+                break;
+            case 'C':
+                tot = 3;
+                break;
+            case 'D':
+                tot = 4;
+                break;
+            default:
+                tot = 5;
+                break;
+        }
+
+        /*
+         반복문
+         */
+
+        int s = 0;
+        for (int i = 1; i <= 100; i++){
+            s = s + i;
+        }
+        System.out.println(s);
+
+        // 조건이 명확할 때는 while () 을 사용하는 편이 좋다
+        int n = 2345;
+        int tol = 0;
+        while (n > 0) {
+            tol = tol + n % 10;
+            n = n / 10;
+        }
+        System.out.println(tol);
     }
+    /*
+        상수 : 상수는 변수와 달리 값이 한 번 결정되면 변경할 수 없다
+    */
+
+    public class SwitchMain{
+        public static final int PHYSICAL = 23;
+    }
+
+
+
 }
