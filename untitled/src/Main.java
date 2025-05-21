fd19f467b575b36e150b7bf6c9ea67502b0b850e
kimhyeok_mac <kimmang940715@naver.com>
2025-05-22 04:36:36 +0900

    스캔 입출력을 통한 기능 구현

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,22 +1,31 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+
+import java.util.Scanner;//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        char sult = 'H';
-        boolean isL = false;
-        int year = 2025;
-        int month = 5;
-        double weight = 80.0d;
-        double height = 177.5d;
+        Scanner scanner = new Scanner(System.in);
 
-        float money = 1.1f;
+        System.out.print("x 값 입력 : ");
+        String strX = scanner.nextLine();
+        int x = Integer.parseInt(strX);
 
-        System.out.println(sult);
-        System.out.println(isL);
-        System.out.println(year);
-        System.out.println(month);
-        System.out.println(weight);
-        System.out.println(height);
-        System.out.println(money);
+        System.out.print("y 값 입력 : ");
+        String strY = scanner.nextLine();
+        int y = Integer.parseInt(strY);
+
+        int result = x + y;
+        System.out.println("x + y  : " + result);
+        System.out.println();
+
+        while(true){
+            System.out.printf("입력 문자열 : ");
+            String data = scanner.nextLine();
+            if(data.equals("q")){
+                break;
+            }
+            System.out.println("출력 문자열 : " + data);
+            System.out.println();
+        }
+        System.out.println("종료");
     }
 }
